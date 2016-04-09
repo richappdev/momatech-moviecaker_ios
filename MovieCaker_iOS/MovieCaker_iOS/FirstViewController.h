c80f98c2ee20be8767cbb85ace79d4725cba95ca
Albert Hsu <ahsu123@Alberts-Mac.local>
2016-04-09 22:03:08 +0800

    add new files

---
+++ MovieCaker_iOS/MovieCaker_iOS/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  MovieCaker_iOS
+//
+//  Created by Albert Hsu on 3/31/16.
+//  Copyright © 2016 Albert Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ MovieCaker_iOS/MovieCaker_iOS/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  MovieCaker_iOS
+//
+//  Created by Albert Hsu on 3/31/16.
+//  Copyright © 2016 Albert Hsu. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
