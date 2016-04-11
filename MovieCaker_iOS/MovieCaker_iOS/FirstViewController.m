620a2fab53c71e71efb3d73146d84a4bcfd711f1
Albert Hsu <ahsu123@Alberts-Mac.local>
2016-04-11 10:20:50 +0800

    set up inital slider

--- MovieCaker_iOS/MovieCaker_iOS/FirstViewController.m
+++ MovieCaker_iOS/MovieCaker_iOS/FirstViewController.m
@@ -7,15 +7,34 @@
 //
 
 #import "FirstViewController.h"
-
+#import "scrollBoxView.h"
 @interface FirstViewController ()
-
+@property (strong, nonatomic) IBOutlet scrollBoxView *scrollView;
+@property (strong, nonatomic) IBOutlet UIScrollView *imageScroll;
 @end
 
 @implementation FirstViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.scrollView.scrollView = self.imageScroll;
+    
+    self.imageScroll.contentSize = CGSizeMake(2000, self.imageScroll.frame.size.height);
+    self.imageScroll.pagingEnabled = YES;
+    //self.imageScroll.backgroundColor = [UIColor blueColor];
+    self.imageScroll.clipsToBounds = NO;
+    
+    UIImageView *image = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"on.png"]];
+    image.frame = CGRectMake(10, 0, 130, 250);
+    [self.imageScroll addSubview:image];
+    
+    UIImageView *image2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"on.png"]];
+    image2.frame = CGRectMake(160, 0, 130, 250);
+    [self.imageScroll addSubview:image2];
+    
+    UIImageView *image3 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"on.png"]];
+    image3.frame = CGRectMake(310, 0, 130, 250);
+    [self.imageScroll addSubview:image3];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
