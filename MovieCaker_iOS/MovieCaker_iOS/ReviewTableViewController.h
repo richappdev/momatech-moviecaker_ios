44f66a2ce28473680b4f2307ee96720707064759
Albert Hsu <ahsu123@Alberts-Mac.local>
2016-06-27 05:32:40 +0800

    setup movie review tableview

---
+++ MovieCaker_iOS/MovieCaker_iOS/ReviewTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReviewTableViewController.h
+//  MovieCaker_iOS
+//
+//  Created by Albert Hsu on 6/27/16.
+//  Copyright © 2016 Albert Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReviewTableViewController : UITableViewController
+
+@end
--- MovieCaker_iOS/MovieCaker_iOS/reviewController.m
+++ MovieCaker_iOS/MovieCaker_iOS/reviewController.m
@@ -10,6 +10,8 @@
 #import "MainVerticalScroller.h"
 #import "buttonHelper.h"
 #import "UIImage+FontAwesome.h"
+#import "buttonHelper.h"
+#import "ReviewTableViewController.h"
 
 @interface reviewController ()
 @property (strong, nonatomic) IBOutlet UIImageView *bgImage;
@@ -20,6 +22,10 @@
 @property (strong, nonatomic) IBOutlet UITextView *content;
 @property (strong, nonatomic) IBOutlet UIImageView *movieNavIcon;
 @property (strong, nonatomic) IBOutlet UIView *movieNavBg;
+@property (strong, nonatomic) IBOutlet UIView *likeBtn;
+@property (strong, nonatomic) IBOutlet UIView *shareBtn;
+@property (strong, nonatomic) IBOutlet UITableView *reviewTable;
+@property ReviewTableViewController *tableController;
 @end
 
 @implementation reviewController
@@ -48,13 +54,29 @@
     self.movieNavBg.layer.cornerRadius =8;
     self.movieNavBg.backgroundColor = [UIColor colorWithRed:(68/255.0f) green:(85/255.0f) blue:(102/255.0f) alpha:1.0];
     [self.content setEditable:NO];
+    
+    [self addIndexGesture:self.likeBtn];
+    [self addIndexGesture:self.shareBtn];
+    
+    self.tableController = [[ReviewTableViewController alloc] init];
+    self.reviewTable.delegate = self.tableController;
+    self.reviewTable.dataSource = self.tableController;
+    self.tableController.tableView = self.reviewTable;
+    self.reviewTable.scrollEnabled = NO;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(void)addIndexGesture:(UIView*)view{
+    UITapGestureRecognizer *indexTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(indexClick:)];
+    [view addGestureRecognizer:indexTap];
+}
 
+-(void)indexClick:(UITapGestureRecognizer *)sender{
+    [buttonHelper likeShareClick:sender.view];
+}
 /*
 #pragma mark - Navigation
 
