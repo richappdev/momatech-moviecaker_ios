af3e74f3d10ce6b5661083f172152d88ff7fda97
Albert Hsu <ahsu123@Alberts-Mac.local>
2016-09-06 05:01:42 +0800

    finish qrcode display

--- MovieCaker_iOS/MovieCaker_iOS/qrcodeViewController.m
+++ MovieCaker_iOS/MovieCaker_iOS/qrcodeViewController.m
@@ -8,9 +8,11 @@
 
 #import "qrcodeViewController.h"
 #import "MainVerticalScroller.h"
+#import "ZXingObjC.h"
 
 @interface qrcodeViewController ()
 @property MainVerticalScroller *helper;
+@property (strong, nonatomic) IBOutlet UIImageView *qrcode;
 @end
 
 @implementation qrcodeViewController
@@ -22,6 +24,27 @@
     self.helper.nav = self.navigationController;
     [self.helper setupBackBtn2:self];
     [self.helper setupSinglePage:self.view];
+    
+    self.qrcode.layer.borderColor = [UIColor colorWithRed:(77/255.0f) green:(182/255.0f) blue:(172/255.0f) alpha:1].CGColor;
+    self.qrcode.layer.borderWidth = 3.0f;
+    
+    NSDictionary *returnData = [NSKeyedUnarchiver unarchiveObjectWithData:[[NSUserDefaults standardUserDefaults] objectForKey:@"userkey"]];
+
+    NSError *error = nil;
+    ZXMultiFormatWriter *writer = [ZXMultiFormatWriter writer];
+    ZXBitMatrix* result = [writer encode:[[[returnData objectForKey:@"Data"] objectForKey:@"UserId"]stringValue]
+                                  format:kBarcodeFormatQRCode
+                                   width:148
+                                  height:148
+                                   error:&error];
+    if (result) {
+        CGImageRef image = [[ZXImage imageWithMatrix:result] cgimage];
+        self.qrcode.image = [[UIImage alloc] initWithCGImage:image];
+        
+        // This CGImageRef image can be placed in a UIImage, NSImage, or written to a file.
+    } else {
+        NSString *errorMessage = [error localizedDescription];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
