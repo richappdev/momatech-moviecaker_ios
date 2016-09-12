026daf9254b3aacb1988a2979598eafd18db610e
Albert Hsu <ahsu123@Alberts-Mac.local>
2016-09-13 00:23:29 +0800

    set up mytopic slider

--- MovieCaker_iOS/MovieCaker_iOS/myTopicsViewController.m
+++ MovieCaker_iOS/MovieCaker_iOS/myTopicsViewController.m
@@ -10,6 +10,11 @@
 #import "MainVerticalScroller.h"
 
 @interface myTopicsViewController ()
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) IBOutlet UILabel *lOne;
+@property (strong, nonatomic) IBOutlet UILabel *lTwo;
+@property (strong, nonatomic) IBOutlet UIView *bar;
+@property UILabel *current;
 @property MainVerticalScroller *helper;
 @end
 
@@ -22,7 +27,9 @@
     self.helper.nav = self.navigationController;
     [self.helper setupBackBtn2:self];
     [self.helper setupSinglePage:self.view];
-    
+    [self addTap:self.lOne];
+    [self addTap:self.lTwo];
+    self.current = self.lOne;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,5 +39,31 @@
 
 -(void)viewWillAppear:(BOOL)animated{
     self.navigationController.navigationBarHidden = NO;
+}
+-(void)moveBar:(UILabel*)label{
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:0.2];
+    
+    self.bar.frame = CGRectMake(label.frame.origin.x-4, label.frame.origin.y+25, label.frame.size.width+8, 3);
+    
+    [UIView commitAnimations];
+    
+}
+-(void)addTap:(UILabel*)label{
+    label.userInteractionEnabled = YES;
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(preSelected:)];
+    [label addGestureRecognizer:tap];
+    
+}
+-(void)preSelected:(UIGestureRecognizer*)gesture{
+    [self selected:gesture.view];
+}
+-(void)selected:(UILabel*)label{
+    if(label!=self.current){
+        self.current.textColor = [UIColor colorWithRed:(121/255.0f) green:(124/255.0f) blue:(131/255.0f) alpha:1];
+        label.textColor = [UIColor colorWithRed:(77/255.0f) green:(182/255.0f) blue:(172/255.0f) alpha:1];
+        self.current = label;
+    }
+    [self moveBar:label];
 }
 @end
