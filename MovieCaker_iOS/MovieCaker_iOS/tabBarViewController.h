a2677061ac07cdfe5a3c4288478158db8bf35903
Albert Hsu <ahsu123@Alberts-Mac.local>
2016-10-21 03:08:36 +0800

    always go to root when click MY tab

---
+++ MovieCaker_iOS/MovieCaker_iOS/tabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  tabBarViewController.h
+//  MovieCaker_iOS
+//
+//  Created by Albert Hsu on 10/21/16.
+//  Copyright © 2016 Albert Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tabBarViewController : UITabBarController
+
+@end
